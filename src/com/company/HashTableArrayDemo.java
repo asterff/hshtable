ee9fde7af1a3cbb826daaa025fb2ddf6a2e9cb9c
asterff <96940811+asterff@users.noreply.github.com>
2022-05-10 18:21:34 +0600

    main

--- src/com/company/HashTableArrayDemo.java
+++ src/com/company/HashTableArrayDemo.java
@@ -1,5 +1,11 @@
 package com.company;
 
 public class HashTableArrayDemo {
-    public static void
+    public static void main(String[] args) {
+        HashTableArray<String> hm = new HashTableArray<String>(10);
+        hm.put(11,"gulnaz");
+        hm.put(12, "gul");
+        hm.put(13, "naz");
+        System.out.println(hm.get(13));
+    }
 }
